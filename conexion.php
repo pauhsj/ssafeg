6430629ad2d6e57921a0a4b1ae3858e464d99691
pauhsj <hernandezpaulina566@gamil.com>
2025-05-20 20:27:09 -0700

    cambios

---
+++ conexion.php
@@ -0,0 +1,27 @@
+<?php 
+$servername = "localhost";
+$username = "u557447082_9x8vh";
+$password ="~s0yVXwZZ";
+$dbname = "u557447082_safegardedb";
+$conexion = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
+
+/*
+
+USAR ESTE SOLO PARA PRUEBAS LOCALES
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "safegardenbd_local";
+$conexion = new mysqli($servername, $username, $password, $dbname);
+if ($conexion->connect_error) {
+    die("Conexion fallida" . $conexion->connect_error);
+}
+
+*/

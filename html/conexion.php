533aae55a79ba9daf7fc2436b8b1e9658bfa8d1e
pauhsj <hernandezpaulina566@gamil.com>
2025-06-15 19:23:54 -0700

    conn bd

--- html/conexion.php
+++ html/conexion.php
@@ -1,7 +1,7 @@
 <?php 
 $servername = "localhost";
 $username = "u557447082_9x8vh";
-$password ="$afegarden_bm9F8>y";
+$password ='$afegarden_bm9F8>y';
 $dbname = "u557447082_safegardedb";
 $conexion = new mysqli($servername, $username, $password, $dbname);
 

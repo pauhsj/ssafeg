78155c4b56a99d78fd248de6399f2b4a987fd7de
pauhsj <hernandezpaulina566@gamil.com>
2025-06-21 15:58:34 -0700

    prueba

--- registrouser.php
+++ registrouser.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $servername = "localhost";
 $username   = "u557447082_9x8vh";
 $password   = '$afegarden_bm9F8>y';

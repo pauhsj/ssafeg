58bb36bbfe9e54dff92a1d8d9eef43b68cf77da7
pauhsj <hernandezpaulina566@gamil.com>
2025-06-26 23:53:38 -0700

    a ver

--- html/conexion.php
+++ html/conexion.php
@@ -3,13 +3,13 @@ $servername = "localhost";
 $username = "u557447082_9x8vh";
 $password ='$afegarden_bm9F8>y';
 $dbname = "u557447082_safegardendb";
-$conexion = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 
 // Verificar la conexión
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
 
 

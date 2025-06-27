076886a426d7c7c8d56c91ea850055bb68e1916f
pauhsj <hernandezpaulina566@gamil.com>
2025-06-26 19:16:37 -0700

    cambio

--- registrouser.php
+++ registrouser.php
@@ -6,7 +6,7 @@ $success = '';
 
 $servername = "localhost";
 $username = "u557447082_9x8vh";
-$password ='$afegarden_bm9F8>y';
+$password ="safegarden_bm9F8>y";
 $dbname = "u557447082_safegardendb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);

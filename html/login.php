06c82f590fd4123db2a69516caf61ad63a1e28cf
pauhsj <hernandezpaulina566@gamil.com>
2025-06-26 21:02:24 -0700

    faak

--- html/login.php
+++ html/login.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         if (password_verify($contrasena, $hash)) {
             $_SESSION["id_cliente"] = $usuario["id_cliente"];
-            header("Location: dashboard.php");
+            header("Location: dasboard.php");
             exit;
         } else {
             echo "<script>alert('Contraseña incorrecta'); window.location.href = 'login.php';</script>";
@@ -203,7 +203,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       timer: 2000,
       timerProgressBar: true,
       showConfirmButton: false,
-      didClose: () => { window.location.href = 'dashboard.php'; }
+      didClose: () => { window.location.href = 'dasboard.php'; }
     });
   <?php endif; ?>
 </script>
